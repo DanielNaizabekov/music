d45070e3501e9c223b0b783cf0fdfca19c6bd8b9
Daniel <danielnaizabekov@gmail.com>
2021-01-17 15:10:17 +0600

    change api key, added env

--- src/api/index.js
+++ src/api/index.js
@@ -1,9 +1,11 @@
 import axios from 'axios';
 
 const baseURL = 'https://www.googleapis.com/youtube/v3';
+const googleApiKey = process.env.VUE_APP_GOOGLE_API_KEY;
+
 export const configs = {
-  googleApiKey: 'AIzaSyA2L7u0EQ1_GGCXyTjmXdbc2W4Ya4v_lnc',
-}
+  googleApiKey: googleApiKey || '',
+};
 
 export default axios.create({
   baseURL,
